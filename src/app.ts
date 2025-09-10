bf25e82c387b546adb9112d5b686196b75596d82
duraanos <duran364@anadolu.edu.tr>
2025-09-10 17:54:52 +0300

    feat(app): Register product in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
