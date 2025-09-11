f54f4b7368ac8eb058fc296b878405264e664e79
duraanos <duran364@anadolu.edu.tr>
2025-09-11 16:06:50 +0300

    feat(services): Implement addItemToCart logic in cartService.ts

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,5 +1,5 @@
 import { supabase } from '../config/db';
-import { Cart } from '../types/cart';
+import { Cart, AddCartItemInput } from '../types/cart';
 
 export async function createCart(userId: string): Promise<Cart> {
   const { data, error } = await supabase
@@ -10,4 +10,41 @@ export async function createCart(userId: string): Promise<Cart> {
 
   if (error) throw new Error(error.message);
   return data;
+}
+
+export async function addItemtoCart(
+  cartId: string,
+  item: AddCartItemInput
+): Promise<Cart> {
+  const { data: cartData, error: fetchError } = await supabase
+    .from('carts')
+    .select('*')
+    .eq('id', cartId)
+    .single();
+
+  console.log(cartData);
+  if (fetchError || !cartData) throw new Error('Cart not found');
+
+  const updatedItems = [...cartData.items];
+  console.log(updatedItems);
+
+  const existingIndex = updatedItems.findIndex(
+    i => i.product_id === item.product_id
+  );
+  console.log(existingIndex);
+
+  existingIndex >= 0
+    ? (updatedItems[existingIndex].quantity += item.quantity)
+    : updatedItems.push(item);
+
+  const { data: updatedCart, error: updatedError } = await supabase
+    .from('carts')
+    .update({ items: updatedItems, updated_at: new Date().toISOString() })
+    .eq('id', cartId)
+    .select()
+    .single();
+
+  if (updatedError) throw new Error(updatedError.message);
+
+  return updatedCart;
 }
