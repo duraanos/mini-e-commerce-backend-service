b384061dcd3a78e1ff3933e90ca1303255617f59
duraanos <duran364@anadolu.edu.tr>
2025-09-11 18:14:05 +0300

    feat(routes): Use cartController object and add updateCartItem & removeCartItem routes

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-import {
-  createCartController,
-  addItemToCartController,
-} from '../controller/cartController';
+import { cartController } from '../controller/cartController';
 
 const router = express.Router();
 
-router.post('/', createCartController);
-router.post('/:id/items', addItemToCartController);
+router.post('/', cartController.createCart);
+router.post('/:id/items', cartController.addItemToCart);
+router.put('/:id/items/:productId', cartController.updateCartItem);
+router.delete('/:id/items/productId', cartController.removeCartItem);
 
 export default router;
