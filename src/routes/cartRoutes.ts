022658d8e93c0e3bdae0080afc277761786cad56
duraanos <duran364@anadolu.edu.tr>
2025-09-13 15:49:43 +0300

    feat(routes): Register cartController in route definitions

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -5,7 +5,8 @@ const router = express.Router();
 
 router.post('/', cartController.createCart);
 router.post('/:id/items', cartController.addItemToCart);
-router.put('/:id/items/:productId', cartController.updateCartItem);
-router.delete('/:id/items/productId', cartController.removeCartItem);
+router.patch('/:id/items/:productId', cartController.updateCartItem);
+router.delete('/:id/items/:productId', cartController.removeCartItem);
+router.get('/:cartId', cartController.getCart);
 
 export default router;
