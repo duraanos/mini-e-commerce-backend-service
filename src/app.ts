9a729af423ac3299cdea892c6c478f727924ce0d
duraanos <duran364@anadolu.edu.tr>
2025-09-15 13:53:34 +0300

    feat(app): Integrate authRoutes into the main application

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import productRoutes from './routes/productRoutes';
 import cartRoutes from './routes/cartRoutes';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
