474e5da5d4e4702c6cd7242d2368ac838860e4ce
duraanos <duran364@anadolu.edu.tr>
2025-09-15 21:26:37 +0300

    feat(cart): Add authentication middleware to cart routes

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,12 +1,21 @@
 import express from 'express';
 import { cartController } from '../controller/cartController';
+import { authenticate } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
-router.post('/', cartController.createCart);
-router.post('/:id/items', cartController.addItemToCart);
-router.patch('/:id/items/:productId', cartController.updateCartItem);
-router.delete('/:id/items/:productId', cartController.removeCartItem);
+router.post('/', authenticate, cartController.createCart);
+router.post('/:id/items', authenticate, cartController.addItemToCart);
+router.patch(
+  '/:id/items/:productId',
+  authenticate,
+  cartController.updateCartItem
+);
+router.delete(
+  '/:id/items/:productId',
+  authenticate,
+  cartController.removeCartItem
+);
 router.get('/:cartId', cartController.getCart);
 
 export default router;
