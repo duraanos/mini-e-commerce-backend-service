5005f3b65cb3b3165eb7c0e49401e6ded287c40e
duraanos <duran364@anadolu.edu.tr>
2025-09-16 12:47:43 +0300

    Add authentication middleware to getAllOrders route

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { orderController } from '../controller/orderController';
+import { authenticate } from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
 router.post('/', orderController.createOrder);
+router.get('/', authenticate, orderController.getAllOrders);
 
 export default router;
