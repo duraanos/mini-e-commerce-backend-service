ce3b68aca8416ce39406d01f8d63a4fb1a7ca819
duraanos <duran364@anadolu.edu.tr>
2025-09-16 12:12:32 +0300

    feat(order): Integrate orderRoutes into the main application

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import productRoutes from './routes/productRoutes';
 import cartRoutes from './routes/cartRoutes';
 import authRoutes from './routes/authRoutes';
+import orderRoutes from './routes/orderRoutes';
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/cart', cartRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/order', orderRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
