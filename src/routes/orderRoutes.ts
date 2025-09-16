8e9368219c4dce7b8762bfea25073ee023a3fc89
duraanos <duran364@anadolu.edu.tr>
2025-09-16 13:17:46 +0300

    feat(order): Add route for retrieving order details by ID

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.post('/', orderController.createOrder);
 router.get('/', authenticate, orderController.getAllOrders);
+router.get('/:orderId', authenticate, orderController.getOrderById);
 
 export default router;
