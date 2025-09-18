55cff301335a8e41c50fe8364ef783f3aaec1ce9
duraanos <duran364@anadolu.edu.tr>
2025-09-18 13:38:54 +0300

    refactor(auth): Improve token verification logic in authentication middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,8 +1,6 @@
 import { error } from 'console';
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-
-const JWT_SECRET = process.env.JWT_SECRET;
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export const authenticate = async (
   req: Request,
@@ -16,10 +14,19 @@ export const authenticate = async (
 
   const token = autheader?.split(' ')[1];
 
+  if (!token) return res.status(401).json({ error: 'No token provided' });
+
+  const secret = process.env.JWT_SECRET;
+
+  if (!secret) throw new Error('JWT_SECRET not defined in environment');
+
   try {
-    const decoded = jwt.verify(token, JWT_SECRET as string);
-    (req as any).user = decoded;
-    next();
+    const decoded = jwt.verify(token, secret) as JwtPayload;
+
+    if (!decoded)
+      return res.status(403).json({ error: 'Invalid token payload' });
+
+    (req as any).user = { id: decoded.id };
   } catch (err) {
     res.status(401).json({ error: 'Invalid token' });
   }
