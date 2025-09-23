899d9f6aa128c48f9890bc496311162dc9cfb1ac
duraanos <duran364@anadolu.edu.tr>
2025-09-23 12:17:36 +0300

    feature(payment): Implement paymentService for order amount calculation and payment intent creation

---
+++ src/services/paymentService.ts
@@ -0,0 +1,57 @@
+import { supabase } from '../config/db';
+import Stripe from 'stripe';
+import { PaymentIntentResponse } from '../types/payment';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2025-08-27.basil',
+});
+
+export const paymentService = {
+  async calculateAmountForOrder(orderId: string): Promise<number> {
+    const { data: order, error } = await supabase
+      .from('orders')
+      .select('total_price')
+      .eq('id', orderId)
+      .single();
+
+    if (error || !order) throw new Error('Order not found');
+
+    const amount = order.total_price;
+
+    return Math.round(amount * 100);
+  },
+
+  async createPaymentIntent(
+    orderId: string,
+    userId: string
+  ): Promise<PaymentIntentResponse> {
+    const amount = await this.calculateAmountForOrder(orderId);
+    if (amount <= 0) throw new Error('Invalid amount');
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+      metadata: {
+        userId,
+        orderId,
+      },
+      automatic_payment_methods: { enabled: true },
+    });
+
+    await supabase.from('payments').insert([
+      {
+        user_id: userId,
+        order_id: orderId,
+        stripe_payment_id: paymentIntent.id,
+        amount: paymentIntent.amount / 100,
+        currency: paymentIntent.currency,
+        status: paymentIntent.status,
+        metadata: paymentIntent.metadata,
+      },
+    ]);
+
+    return {
+      clientSecret: paymentIntent.client_secret as string,
+    };
+  },
+};
