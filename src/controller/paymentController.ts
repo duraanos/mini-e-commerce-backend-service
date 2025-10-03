0b69eab8c063f3240e1aa94915eb0a1a3c743fa6
duraanos <duran364@anadolu.edu.tr>
2025-10-03 16:16:40 +0300

    feat(payment): Implement paymentController for handling PayPal payment creation

---
+++ src/controller/paymentController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { paymentService } from '../services/paymentService';
+import { PaymentRequest } from '../types/payment';
+
+export const paymentController = {
+  async handleCreatePayment(req: Request, res: Response): Promise<void> {
+    try {
+      const { userId } = (req as any).user?.id;
+      const { orderId } = req.body as PaymentRequest;
+
+      if (!orderId) res.status(404).json({ error: 'Order ID not found' });
+
+      const paymentResponse = await paymentService.createPayPalOrder(
+        userId,
+        orderId
+      );
+
+      res.json(paymentResponse);
+    } catch (err: unknown) {
+      res
+        .status(500)
+        .json({ error: err instanceof Error ? err.message : String(err) });
+    }
+  },
+};
