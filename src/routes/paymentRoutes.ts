1c4bec6d2090f51fcc3a39d8d8706abd9d70ed3e
duraanos <duran364@anadolu.edu.tr>
2025-10-10 19:35:04 +0300

    feat(routes): Add validation middleware for payment creation

--- src/routes/paymentRoutes.ts
+++ src/routes/paymentRoutes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { paymentController } from '../controller/paymentController';
 import { authenticate } from '../middlewares/authMiddleware';
+import { orchestratorSchema, validate } from '../validators/paymentValidator';
 
 const router = express.Router();
 
-router.post('/create', authenticate, paymentController.createPayment);
+router.post(
+  '/create',
+  authenticate,
+  validate(orchestratorSchema),
+  paymentController.createPayment
+);
 
 export default router;
