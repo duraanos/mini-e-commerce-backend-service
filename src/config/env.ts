84de9efe7acada624fe38437bc3734266d1b31e0
duraanos <duran364@anadolu.edu.tr>
2025-10-10 11:27:23 +0300

    fix(env): Add missing PAYPAL_BASE_URL to environment variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -6,5 +6,6 @@ export const env = {
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID!,
   PAYPAL_CLIENT_SECRET: process.env.PAYPAL_CLIENT_SECRET!,
   PAYPAL_WEBHOOK_ID: process.env.PAYPAL_WEBHOOK_ID!,
+  PAYPAL_BASE_URL: process.env.PAYPAL_BASE_URL!,
   PAYPAL_MODE: process.env.PAYPAL_MODE!,
 };
