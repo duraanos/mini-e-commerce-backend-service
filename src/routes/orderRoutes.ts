ecf22c976da1e38f84c4e9984e78eb19eff3b1c6
duraanos <duran364@anadolu.edu.tr>
2025-10-10 19:33:42 +0300

    feat(routes): Add validation for order creation, retrieval, update, and deletion

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -1,13 +1,39 @@
 import express from 'express';
 import { orderController } from '../controller/orderController';
 import { authenticate } from '../middlewares/authMiddleware';
+import {
+  createOrderSchema,
+  getOrderByIdSchema,
+  updateOrderSchema,
+  validate,
+} from '../validators/orderValidator';
 
 const router = express.Router();
 
-router.post('/', authenticate, orderController.createOrder);
+router.post(
+  '/',
+  authenticate,
+  validate(createOrderSchema),
+  orderController.createOrder
+);
 router.get('/', authenticate, orderController.getAllOrders);
-router.get('/:orderId', authenticate, orderController.getOrderById);
-router.patch('/:orderId', authenticate, orderController.updateOrder);
-router.delete('/:orderId', authenticate, orderController.deleteOrder);
+router.get(
+  '/:orderId',
+  authenticate,
+  validate(getOrderByIdSchema),
+  orderController.getOrderById
+);
+router.patch(
+  '/:orderId',
+  authenticate,
+  validate(updateOrderSchema),
+  orderController.updateOrder
+);
+router.delete(
+  '/:orderId',
+  authenticate,
+  validate(getOrderByIdSchema),
+  orderController.deleteOrder
+);
 
 export default router;
