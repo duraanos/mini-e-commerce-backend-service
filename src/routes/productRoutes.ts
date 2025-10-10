e72fd602762cd307ab378b565f2d4ff15c116d7b
duraanos <duran364@anadolu.edu.tr>
2025-10-10 19:31:38 +0300

    feat(routes): Add validation for product creation, update, and deletion

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,13 +1,34 @@
 import express from 'express';
 import { productController } from '../controller/productController';
 import { authenticate } from '../middlewares/authMiddleware';
+import {
+  productSchema,
+  updateProductSchema,
+  productIdSchema,
+  validate,
+} from '../validators/productValidator';
 
 const router = express.Router();
 
-router.post('/', authenticate, productController.createProduct);
+router.post(
+  '/',
+  authenticate,
+  validate(productSchema),
+  productController.createProduct
+);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.put('/:id', authenticate, productController.updateProduct);
-router.delete('/:id', authenticate, productController.deleteProduct);
+router.put(
+  '/:id',
+  authenticate,
+  validate(updateProductSchema),
+  productController.updateProduct
+);
+router.delete(
+  '/:id',
+  authenticate,
+  validate(productIdSchema),
+  productController.deleteProduct
+);
 
 export default router;
