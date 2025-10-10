f7f7e069de0b8772e2ddc0535b3ca9a108c56290
duraanos <duran364@anadolu.edu.tr>
2025-10-10 19:27:29 +0300

    feat(routes): Add validation middleware for registration and login routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import { authContoller } from '../controller/authController';
+import {
+  registerSchema,
+  loginSchema,
+  validate,
+} from '../validators/authValidator';
 
 const router = express.Router();
 
-router.post('/register', authContoller.registerUser);
-router.post('/login', authContoller.loginUser);
+router.post('/register', validate(registerSchema), authContoller.registerUser);
+router.post('/login', validate(loginSchema), authContoller.loginUser);
 
 export default router;
