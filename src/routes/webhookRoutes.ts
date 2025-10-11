0f0b11e77066eecd4ed576c31eafe9874f031ceb
duraanos <duran364@anadolu.edu.tr>
2025-10-11 18:17:39 +0300

    feat(routes): Add webhook validation middleware

--- src/routes/webhookRoutes.ts
+++ src/routes/webhookRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { webhookController } from '../controller/webhookController';
+import { webhookValidator } from '../validators/webhookValidator';
 
 const router = express.Router();
 
-router.post('/:provider', webhookController.handleWebhook);
+router.post('/:provider', webhookValidator, webhookController.handleWebhook);
 
 export default router;
